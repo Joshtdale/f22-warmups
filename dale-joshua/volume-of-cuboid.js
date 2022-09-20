f726f4e872552dcb2e99191d75c2abadc356c082
Josh <111532201+Joshtdale@users.noreply.github.com>
2022-09-20 09:46:19 -0400

    Create volume-of-cuboid.js

---
+++ dale-joshua/volume-of-cuboid.js
@@ -0,0 +1,10 @@
+// Josh and Daniel
+
+// get values from l w h
+// return vol
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    let volume = (length * width * height);
+    return volume;
+  }
+}
